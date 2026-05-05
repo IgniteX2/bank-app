7e24a67aba8dff3d69fa60e673d21a7791d861d5
feraquzzi <mosesobembe@gmail.com>
2026-05-05 12:30:30 +0100

    Initialized the mock-bank-app file structure to the main base

---
+++ src/components/Register.jsx
@@ -0,0 +1,7 @@
+// import { registerUser } from "../services/authService";
+
+function Register() {
+  return <div></div>;
+}
+
+export default Register;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import React from "react";
+import { StrictMode } from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
-)
+);

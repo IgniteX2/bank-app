b25476013ec094904aef18676459c4f0a5292bae
jval-0 <justinvalchukwuemeka@gmail.com>
2026-05-07 09:49:25 +0100

    Backend Authentication for Login and Signup

---
+++ Mini_Digital_Banking_system/src/main/java/IgniteX_Project1/Mini_Digital_Banking_system/Model/Request/LogInRequest.java
@@ -0,0 +1,15 @@
+package IgniteX_Project1.Mini_Digital_Banking_system.Model.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class LogInRequest {
+
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    private String password;
+}
---
+++ Mini_Digital_Banking_system/src/main/java/IgniteX_Project1/Mini_Digital_Banking_system/Service/AuthService.java
@@ -0,0 +1,65 @@
+package IgniteX_Project1.Mini_Digital_Banking_system.Service;
+
+import IgniteX_Project1.Mini_Digital_Banking_system.Exceptions.InvalidCredentialException;
+import IgniteX_Project1.Mini_Digital_Banking_system.Exceptions.UserAlreadyExistsException;
+import IgniteX_Project1.Mini_Digital_Banking_system.Model.Request.AccountCreationRequest;
+import IgniteX_Project1.Mini_Digital_Banking_system.Model.Request.LogInRequest;
+import IgniteX_Project1.Mini_Digital_Banking_system.Model.UserInfo;
+import IgniteX_Project1.Mini_Digital_Banking_system.repository.UserRepository;
+import IgniteX_Project1.Mini_Digital_Banking_system.security.JwtUtil;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuthService {
+    private final UserRepository repo;
+    private final BCryptPasswordEncoder encoder;
+    private  final JwtUtil jwtUtil;
+
+    public AuthService(UserRepository repo,
+                       BCryptPasswordEncoder encoder,
+                       JwtUtil jwtUtil) {
+        this.repo = repo;
+        this.encoder = encoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+
+
+    @Transactional
+    public String signup(AccountCreationRequest req) {
+
+        String fullName = req.getFirstName() + " " + req.getLastName();
+        if (repo.findByFullName(fullName).isPresent() || repo.findByEmail(req.getEmail()).isPresent()) {
+            throw new UserAlreadyExistsException("user already exist");
+        }
+
+        UserInfo userInfo = new UserInfo();
+//        userInfo.setUser_id(req.getId());
+        userInfo.setFirstName(req.getFirstName());
+        userInfo.setLastName(req.getLastName());
+//        userInfo.setUsername(req.getUsername());
+        userInfo.setEmail(req.getEmail());
+        userInfo.setPhoneNumber(req.getPhoneNumber());
+        userInfo.setBvn(req.getBvn());
+        userInfo.setUserPassword(encoder.encode(req.getUserPassword()));
+        userInfo.setNINum(req.getNInNum());
+
+        repo.save(userInfo);
+
+        return "User registered Successfully";
+    }
+
+    public String login(LogInRequest req) {
+
+        UserInfo user = repo.findByFullName(req.getFullName())
+                .orElseThrow(() ->
+                        new InvalidCredentialException("invalid username or password"));
+        if (!encoder.matches(req.getPassword(), user.getUserPassword())) {
+             throw new InvalidCredentialException("invalid username or password");
+        }
+        return  jwtUtil.generateToken(user.getFullName());
+    }
+
+}

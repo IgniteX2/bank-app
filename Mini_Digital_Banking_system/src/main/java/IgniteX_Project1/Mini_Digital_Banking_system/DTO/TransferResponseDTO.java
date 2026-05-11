e763cb3cd33be6ae10bbba74066c3fc52aaa95ec
Anthony-coder628 <anthonyegechi@gmail.com>
2026-05-11 15:03:59 +0100

    Updated the name for Transaction table

---
+++ Mini_Digital_Banking_system/src/main/java/IgniteX_Project1/Mini_Digital_Banking_system/DTO/TransferResponseDTO.java
@@ -0,0 +1,4 @@
+package IgniteX_Project1.Mini_Digital_Banking_system.DTO;
+
+public class TransferResponseDTO {
+}
---
+++ Mini_Digital_Banking_system/src/main/java/IgniteX_Project1/Mini_Digital_Banking_system/Exceptions/InsufficientFundsException.java
@@ -0,0 +1,4 @@
+package IgniteX_Project1.Mini_Digital_Banking_system.Exceptions;
+
+public class InsufficientFundsException {
+}

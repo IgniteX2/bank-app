4906506ba8e3d58ed03b4797dafd6cfcb56ac818
jval-0 <justinvalchukwuemeka@gmail.com>
2026-05-15 15:49:44 +0100

    add to main so i can run it on my system

--- Mini_Digital_Banking_system/src/main/java/IgniteX_Project1/Mini_Digital_Banking_system/config/SecurityConfig.java
+++ Mini_Digital_Banking_system/src/main/java/IgniteX_Project1/Mini_Digital_Banking_system/config/SecurityConfig.java
@@ -12,11 +12,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-//import java.util.List;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -54,9 +54,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http ) throws Exception{
         http
-                .cors(Customizer.withDefaults())
+//                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS
@@ -75,43 +75,48 @@ public class SecurityConfig {
                             res.setContentType("application/json");
                             res.getWriter().write("{\"error\":\"Unauthorized\"}");
                         })
+                        .accessDeniedHandler(((request, response, accessDeniedException) -> {
+                            response.setStatus(403);
+                            response.setContentType("application/json");
+                            response.getWriter().write("{\"error\":\"Forbidden\"}");
+                        }))
                 )
 
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//
-//        configuration.setAllowedOriginPatterns(
-//                List.of("*")
-//        );
-//
-//        configuration.setAllowedMethods((
-//                List.of(
-//                        "GET",
-//                        "POST",
-//                        "PUT",
-//                        "DELETE",
-//                        "OPTIONS"
-//                )
-//                ));
-//
-//        configuration.setAllowedHeaders(
-//                List.of("Authorization", "Cache-Control","Content-Type")
-//        );
-//
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//        source.registerCorsConfiguration(
-//                "/**",
-//                configuration
-//        );
-//
-//        return source;
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(
+                List.of("*")
+        );
+
+        configuration.setAllowedMethods((
+                List.of(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                ));
+
+        configuration.setAllowedHeaders(
+                List.of("Authorization","Content-Type")
+        );
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
+    }
 }

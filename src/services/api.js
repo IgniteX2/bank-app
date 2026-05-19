80613dc8148dd1fbe812fc35ecac7acd1111463b
Anthony-coder628 <anthonyegechi@gmail.com>
2026-05-19 20:52:02 +0100

    My updated base_url

--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://schedule-tall-throwback.ngrok-free.dev",
+    baseURL: "http://localhost:8083",
 });
 
 // Attach token to every request
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
+    return config;
 });
 
 // Handle unauthorized responses
 API.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/login";
-    }
+    (res) => res,
+    (err) => {
+        if (err.response?.status === 401) {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+        }
 
-    return Promise.reject(err);
-  },
+        return Promise.reject(err);
+    },
 );
 
 export default API;
